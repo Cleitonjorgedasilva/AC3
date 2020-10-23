d3b0e6d48db8815ab55c208ba1b4da0b92e52191
Cleitonjorgedasilva <cleiton.silva@aluno.faculdadeimpacta.com.br>
2020-10-23 01:01:52 -0300

    AC3

--- src/Primos.py
+++ src/Primos.py
@@ -18,5 +18,5 @@ def primos():
 print (primos())
 
 if __name__ == "__main__":
-    port = int(os.eviron.get("PORT, 5000"))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
